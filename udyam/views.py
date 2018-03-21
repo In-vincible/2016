c249e8c909b9cd51d217f5a58a6281426d1b4af6
KnightSky <akash.gupta.ece15@itbhu.ac.in>
2018-03-21 21:18:16 +0530

    ch_4

--- udyam/views.py
+++ udyam/views.py
@@ -16,7 +16,7 @@ def index(request):
             team.contact = json.dumps(form_detail[u'main_contact'])
             team.team_name = form_detail[u'team_name']
             team.team_details = form_detail[u'team_details']
-            #team.save()
+            team.save()
             #mail = SendMail(form_detail)
             #mail.mail_coordinator()
             #mail.mail_representative()
