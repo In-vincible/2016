f135df93de13dced8c92b0cfa7f66c1b981bea03
KnightSky <akash.gupta.ece15@itbhu.ac.in>
2018-03-21 21:11:38 +0530

    ch_2

--- udyam/views.py
+++ udyam/views.py
@@ -8,20 +8,20 @@ from .send_mail import SendMail
 def index(request):
     if request.method == 'POST':
         error = 0
-        if True:
+        try:
             #print (request.body)
             form_detail = json.loads(request.body.decode("utf-8"))
-            team = RegistrationInfo()
-            team.event_name = form_detail[u'event']
-            team.contact = json.dumps(form_detail[u'main_contact'])
-            team.team_name = form_detail[u'team_name']
-            team.team_details = form_detail[u'team_details']
-            team.save()
+            #team = RegistrationInfo()
+            #team.event_name = form_detail[u'event']
+            #team.contact = json.dumps(form_detail[u'main_contact'])
+            #team.team_name = form_detail[u'team_name']
+            #team.team_details = form_detail[u'team_details']
+            #team.save()
             #mail = SendMail(form_detail)
             #mail.mail_coordinator()
             #mail.mail_representative()
 
-        else:
+        except:
 
             error = 1
             #print(type(inst))
